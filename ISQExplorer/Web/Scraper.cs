fbc999c3a1089e7550828efe5791779f9fd5b9ff
Jonathan Lemos <jonathanlemos@protonmail.com>
2020-03-01 23:43:38 -0500

    scraper caches parsed html now. uses new html client

--- ISQExplorer/Web/Scraper.cs
+++ ISQExplorer/Web/Scraper.cs
@@ -11,6 +11,7 @@ using ISQExplorer.Functional;
 using ISQExplorer.Misc;
 using ISQExplorer.Models;
 using ISQExplorer.Repositories;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore.Internal;
 
 namespace ISQExplorer.Web
@@ -34,13 +35,26 @@ namespace ISQExplorer.Web
         public ConcurrentDictionary<string, CourseModel> Courses { get; set; }
         public ConcurrentBag<(DepartmentModel, TermModel, CourseScrapeException)> CourseScrapeErrors;
 
-        public HtmlPage? IndexPageCache { get; set; }
-        public ConcurrentDictionary<(DepartmentModel, TermModel), HtmlPage> DepartmentListCache { get; set; }
-        public ConcurrentDictionary<ProfessorModel, HtmlPage> ProfessorListCache { get; set; }
+        private readonly ConcurrentDictionary<(Either<string, Uri>, string?), HtmlPage> PageCache;
 
-        private async Task<Try<HtmlPage, IOException>> PageFromUrl(Either<string, Uri> url, string? postData) =>
-            await (await (postData == null ? _htmlClient.GetAsync(url) : _htmlClient.PostAsync(url, postData)))
-                .SelectAsync(HtmlPage.FromHtmlAsync);
+        private async Task<Try<HtmlPage, IOException>> PageFromUrl(Either<string, Uri> url, string? postData = null)
+        {
+            if (PageCache.ContainsKey((url, postData)))
+            {
+                return PageCache[(url, postData)];
+            }
+
+            var res =
+                await (await (postData == null ? _htmlClient.GetAsync(url) : _htmlClient.PostAsync(url, postData)))
+                    .SelectAsync(HtmlPage.FromHtmlAsync);
+
+            if (res.HasValue)
+            {
+                PageCache[(url, postData)] = res.Value;
+            }
+
+            return res;
+        }
 
 
         public Scraper(ITermRepository termRepo, IHtmlClient htmlClient)
@@ -48,44 +62,25 @@ namespace ISQExplorer.Web
             _htmlClient = htmlClient;
             Departments = new ConcurrentSet<DepartmentModel>();
             Terms = termRepo;
-
-            DepartmentListCache = new ConcurrentDictionary<(DepartmentModel, TermModel), HtmlPage>();
-            ProfessorListCache = new ConcurrentDictionary<ProfessorModel, HtmlPage>();
         }
 
         public Task<Result> ScrapeDepartmentsAsync() => Result.OfAsync(async () =>
         {
-            if (IndexPageCache == null)
-            {
-                IndexPageCache = (await PageFromUrl(Urls.DeptSchedule))).Value;
-            }
-
-            IndexPageCache.Query<IHtmlSelectElement>("#dept_id").Value
+            (await PageFromUrl(Urls.DeptSchedule)).Value.Query<IHtmlSelectElement>("#dept_id").Value
                 .Children<IHtmlOptionElement>().Value.Skip(1)
                 .ForEach(e => Departments.Add(new DepartmentModel {Id = Parse.Int(e.Id).Value, Name = e.Label}));
         });
 
         public Task<Result> ScrapeTermsAsync() => Result.OfAsync(async () =>
         {
-            if (IndexPageCache == null)
-            {
-                IndexPageCache = (await _limiter.Run(() => HtmlPage.FromUrlAsync(Urls.DeptSchedule))).Value;
-            }
-
-            IndexPageCache.Query<IHtmlSelectElement>("#term_id").Value
+            (await PageFromUrl(Urls.DeptSchedule)).Value.Query<IHtmlSelectElement>("#term_id").Value
                 .Children<IHtmlOptionElement>().Value.Skip(1)
                 .ForEach(e => Terms.Add(new TermModel {Name = e.Label, Id = Parse.Int(e.Value).Value}));
         });
 
         public Task<Result> ScrapeCourses(DepartmentModel dept, TermModel term) => Result.OfAsync(async () =>
         {
-            if (!DepartmentListCache.ContainsKey((dept, term)))
-            {
-                DepartmentListCache[(dept, term)] = (await _limiter.Run(() =>
-                    HtmlPage.FromUrlAsync(Urls.DeptSchedule, Urls.DeptSchedulePostData(term.Id, dept.Id)))).Value;
-            }
-
-            var page = DepartmentListCache[(dept, term)];
+            var page = (await PageFromUrl(Urls.DeptSchedule, Urls.DeptSchedulePostData(term.Id, dept.Id))).Value;
             var tables = page.QueryAll<IHtmlTableElement>("table.datadisplaytable").ToList();
             if (tables.Count != 3)
             {
@@ -132,13 +127,7 @@ namespace ISQExplorer.Web
 
         public Task<Result> ScrapeProfessors(DepartmentModel dept, TermModel term) => Result.OfAsync(async () =>
         {
-            if (!DepartmentListCache.ContainsKey((dept, term)))
-            {
-                DepartmentListCache[(dept, term)] = (await _limiter.Run(() =>
-                    HtmlPage.FromUrlAsync(Urls.DeptSchedule, Urls.DeptSchedulePostData(term.Id, dept.Id)))).Value;
-            }
-
-            var page = DepartmentListCache[(dept, term)];
+            var page = (await PageFromUrl(Urls.DeptSchedule, Urls.DeptSchedulePostData(term.Id, dept.Id))).Value;
             var tables = page.QueryAll<IHtmlTableElement>("table.datadisplaytable").ToList();
             if (tables.Count != 3)
             {
@@ -174,7 +163,7 @@ namespace ISQExplorer.Web
 
                 if (!Professors.ContainsKey((dept, lname)))
                 {
-                    var page = (await HtmlPage.FromUrlAsync(Urls.ProfessorPage(nNumber.Value))).Value;
+                    var page = (await PageFromUrl(Urls.ProfessorPage(nNumber.Value))).Value;
 
                     var professorName = Try.Of(() => page.QueryAll<IHtmlTableCellElement>("td.dddefault").First(
                         elem => elem.PreviousElementSibling?.TextContent.HtmlDecode().Trim() == "Instructor:"));
@@ -193,17 +182,10 @@ namespace ISQExplorer.Web
                         LastName = lname,
                         NNumber = nNumber.Value
                     };
-
-                    ProfessorListCache[Professors[(dept, lname)]] = page;
                 }
             });
         });
 
-        public Task<Result> ScrapeEntries(ProfessorModel prof) => Result.OfAsync(async () =>
-        {
-            if (!ProfessorListCache.ContainsKey(prof))
-            {
-            }
-        });
+        public Task<Result> ScrapeEntries(ProfessorModel prof) => Result.OfAsync(async () => { });
     }
 }
